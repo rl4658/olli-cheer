71bbcfde156a8be9bbfb458061fd6d29bcfe4185
Raymond <rli684@uwo.ca>
2024-02-04 01:14:18 -0500

    Edited About Us

--- olli/src/components/AboutUS/AboutUsSection.js
+++ olli/src/components/AboutUS/AboutUsSection.js
@@ -2,25 +2,22 @@ import React from 'react';
 import '../../CSS/AboutUs/AboutUs.css'; // css file
 
 const AboutUs = () => {
- return (
+  return (
     <div className="about-us-container">
       <div className="content-container">
-        <div className="image-container">
-          <img src={require("./cheerconnections.png")} />
+        <div className="text-container">
+          <h1 data-aos="fade-right" className="heading">Our Vision</h1>
         </div>
         <div className="mission-statement">
-          <h1 className="heading">Welcome to Our World of Care</h1>
-          <p>
-            At Ongoing Living & Learning Inc. (OLLI), we are dedicated to creating
-            an inclusive community and a circle of friendship. We focus on
-            empowering and enriching the lives of individuals with intellectual
+          <p data-aos="fade-left">
+            We believe in empowering and enriching the lives of individuals with intellectual
             disabilities and their families through development, social
             recreation, and community integration opportunities.
           </p>
         </div>
       </div>
     </div>
- );
+  );
 };
 
 export default AboutUs;
